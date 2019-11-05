4e68f911f7e17359fe78a06deaaa88c4e9940e0e
qiaoshh <shhqiao@163.com>
2019-11-05 15:35:06 +0800

    pelee-ssd

--- git_upload_repository.sh
+++
@@ -1,8 +0,0 @@
-******upload repository to github******
-1. git init 
-2. git add . # add all files to repository
-3. git commit -m "repository name"
-4. login github create new repository
-5. git remote add origin https://github.com/qiaoshh/**
-6. git pull origin master
-7. git push -u origin master
