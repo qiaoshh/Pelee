3f056ac3ea48afdc319f30eda933dc95acab5f71
Qiao Shaohua <31871418+qiaoshh@users.noreply.github.com>
2019-11-05 16:49:38 +0800

    Update train_pelee_ssd_dc.sh

--- train_pelee_ssd_dc.sh
+++ train_pelee_ssd_dc.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 LOG=log/test_`date +%Y-%m-%d-%H-%M-%S`.log
-./build/tools/caffe train --solver /home/qiao/caffe-ssd/examples/pelee-ssd300_double_camera/solver.prototxt --gpu=0 2>&1 | tee $LOG
+./build/tools/caffe train --solver /your/own/caffe/path/examples/pelee-ssd300_double_camera/solver.prototxt --gpu=0 2>&1 | tee $LOG
 
